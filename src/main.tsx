d8ec36ecdc8e4a9d31daa2e3c4b8fdfa28b4df02
Mujahid Rafiq <mujahidrafiqch@gmail.com>
2026-08-25 14:33:26 +0500

    replaced hash router to browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
-// 
-
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import App from "./App";
@@ -22,9 +20,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </QueryClientProvider>
   </Provider>
 );
