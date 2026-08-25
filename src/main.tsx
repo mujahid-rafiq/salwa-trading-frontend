d43f4bbbab4835f90f20b00ffd402a2fdc44afeb
Mujahid Rafiq <mujahidrafiqch@gmail.com>
2026-08-25 14:02:43 +0500

    fix: switch to HashRouter to resolve 404 on direct route access

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
+// 
+
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import App from "./App";
@@ -20,9 +22,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </QueryClientProvider>
   </Provider>
 );
