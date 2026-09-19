8f66393ff82a18c3f06383981a1d64c802bf594d
Mujahid Rafiq <mujahidrafiqch@gmail.com>
2026-09-19 09:55:47 -0700

    fixes issues

--- src/components/packages/PackageCard.tsx
+++ src/components/packages/PackageCard.tsx
@@ -68,7 +68,7 @@ const PackageCard: React.FC<PackageCardProps> = ({
           </div>
         </div>
 
-        {/* Features are */}
+        {/* Features are... */}
         <div className="mt-8 space-y-3.5">
           {features.map((feature, index) => (
             <div
